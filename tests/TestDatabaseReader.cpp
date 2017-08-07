2b77018acc1cc6172ae3975401fd87b8a57a08ed
Gabe Gordon <gabegordon1234@gmail.com>
2017-08-07 13:38:58 -0400

    Fix travis directory location

--- tests/TestDatabaseReader.cpp
+++ tests/TestDatabaseReader.cpp
@@ -3,5 +3,9 @@
 
 TEST_CASE("DatabaseReader Checks")
 {
-
+    SECTION("Database All APIDS")
+    {
+        std::vector<uint32_t> apids;
+        DatabaseReader dr(true, DataTypes::J1, apids);
+    }
 }
